5a99de8835f27537c55b50f6bcb34dd98188c01d
Sadaananth Anbucheliyan <ananth.anbucheliyan@gmail.com>
2023-04-14 22:25:21 +0800

    Added dependencies

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,14 +1,110 @@
-#include <iostream>
-
 #include "Game.hpp"
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <iostream>
+#include <fstream>
+#include <stdexcept>
+#include <string>
+#include <sstream>
+
+#define ASSERT(x) if(!(x)) __debugbreak();
+#define GLCall(x) GLClearError();\
+	x;\
+	ASSERT(GLLogCall(#x, __FILE__, __LINE__))
+
+static void GLClearError()
+{
+	while (glGetError() != GL_NO_ERROR);
+}
+
+
+static bool GLLogCall(const char* func, const char* file, int line)
+{
+	while (auto error = glGetError()) {
+		std::cout << file << ":" << func << ":" << line << " GL error " << error << std::endl;
+		return false;
+	}
+
+	return true;
+}
+
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+float deltaTime = 0.0f; // Time between current frame and last frame
+float lastFrame = 0.0f; // Time of last frame
+float lastX = 400, lastY = 400;
+float yaw = -90.0f;
+float pitch = 0.0f;
+float Zoom = 45.0f;
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		std::cout << "Escape pressed" << std::endl;
+		glfwSetWindowShouldClose(window, true);
+	}
+	const float cameraSpeed = 0.25f * deltaTime; // adjust accordingly
+	if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+		cameraPos += cameraSpeed * cameraFront;
+	if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+		cameraPos -= cameraSpeed * cameraFront;
+	if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
+		cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) *
+		cameraSpeed;
+	if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
+		cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) *
+		cameraSpeed;
+
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	std::cout << "Width " << width << " Height " << height << std::endl;
+	glViewport(0, 0, width, height);
+}
 
 int main()
 {
-	Game game;
+	glfwInit();
 
-	while (true)
-	{
-		game.render();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 800, "Opengl", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Window is NULL " << std::endl;
+		glfwTerminate();
+		return -1;
 	}
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	while (!glfwWindowShouldClose(window)) {
+		GLCall(glClear(GL_COLOR_BUFFER_BIT));
+
+		processInput(window);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
 	return 0;
 }
